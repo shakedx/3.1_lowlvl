587d85e15c6b38f73ae064a13a7069e4c75ad0bb
shakedx <kolesnikov_danil123@mail.ru>
2021-11-03 13:20:32 +0200

    Добавьте файлы проекта.

---
+++ 3.1_lowlvl/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Kolesnikov_3._2_baza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите m:");
+                int m = int.Parse(Console.ReadLine());
+                double R = 0;
+                int count = m;
+                {
+                    R = m * m - 2 * m * n + n * n;
+                }
+                Console.WriteLine($"R={R}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
